5b11c2a373c64774952ac3487668e6ff6f39a7f5
Edsson <e_d_sx2@hotmail.com>
2024-05-15 14:40:51 -0600

    added a second rover to the form

--- src/components/userInput/userInput.js
+++ src/components/userInput/userInput.js
@@ -17,10 +17,17 @@ export default function UserInput({sendInfo}){
         <section className="form">
             <label htmlFor="plateau">Set the size of the plateau: </label>
             <input type="text" placeholder="Example: 5 5" id="plateau"/>
-            <label htmlFor="roverInitialCoordinate">Set the inicial coordinate of the 1st rover:</label>
+
+            <label htmlFor="roverInitialCoordinate">Set the initial coordinate of the 1st rover:</label>
             <input type="text" placeholder="Example: 1 2 N" id="roverInitialCoordinate" onChange={getInitialCoordinatesRoverOne}/>
             <label htmlFor="roverMovement">Set the movement of the 1st rover:</label>
-            <input type="text" placeholder="Example: LMLMLMLMM" id="roverMovement"/>           
+            <input type="text" placeholder="Example: LMLMLMLMM" id="roverMovement"/> 
+
+            <label htmlFor="secondRoverInitialCoordinate">Set the initial coordinate of the 2nd rover:</label>
+            <input type="text" placeholder="Example: 1 2 N" id="secondRoverInitialCoordinate" onChange={getInitialCoordinatesRoverOne}/>
+            <label htmlFor="secondRoverMovement">Set the movement of the 2nd rover:</label>
+            <input type="text" placeholder="Example: LMLMLMLMM" id="secondRoverMovement"/>
+            <button>Calculate</button>          
         </section>
     )
 }
